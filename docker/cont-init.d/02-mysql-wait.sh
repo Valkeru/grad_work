94dff04769c2f18131c820c7640df14896f0b27f
Igor Lis <valkeru@valkeru.ru>
2018-08-20 22:11:34 +0300

    IP хоста для xdebug, костыль для миграций

--- docker/cont-init.d/02-mysql-wait.sh
+++ docker/cont-init.d/02-mysql-wait.sh
@@ -6,4 +6,8 @@ while ! nc -z $DB_HOST $DB_PORT; do
   sleep 0.1
 done
 
+# ХЗ, что за дичь, но иначе миграции падают
+# Ждём, пока всё полностью поднимется
+sleep 20
+
 echo "MySQL started"
--- docker/cont-init.d/06-migrations.sh
+++ docker/cont-init.d/06-migrations.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/with-contenv bash
 
-cd /var/www
-
-s6-setuidgid www-data php bin/console doctrine:migration:migrate -qn
+#cd /var/www
+#
+#s6-setuidgid www-data php bin/console doctrine:migration:migrate -qn
